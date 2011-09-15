5630801ea56336ce0b1459e8945934953cace6a5
Rob Rosenbaum <rob@robrosenbaum.com>
2011-09-15 09:41:00 -0400

    adding input read, and keyboard-specific config

--- chordite.c
+++ chordite.c
@@ -2,19 +2,63 @@
 
 #define MAX_SNAPSHOTS 1000
 #define MAX_OUTPUT 10
-#define LAYOUT_SIZE 50
 
 #define MALLOCS(t,s) (t *) myalloc(s * sizeof(t))
 #define MALLOC(t) (t *) myalloc(sizeof(t))
 
+
+/** KEYBOARD-SPECIFIC CONFIGURATION **/
+
+#define LAYOUT_SIZE 50
+
+#define PINKY_L 0
+#define PINKY_H 1
+#define RING_L 2
+#define RING_H 3
+#define MIDDLE_L 4
+#define MIDDLE_H 5
+#define INDEX_L 7
+#define INDEX_H 8
+
+#define UP LOW
+#define DOWN HIGH
+
+#define NUM_SWITCHES_I 2
+#define NUM_SWITCHES_M 2
+#define NUM_SWITCHES_R 2
+#define NUM_SWITCHES_P 2
+
+const int SWITCHES_I[NUM_SWITCHES_I] = {
+  INDEX_L,
+  INDEX_H
+};
+const int SWITCHES_M[NUM_SWITCHES_M] = {
+  MIDDLE_L,
+  MIDDLE_H
+};
+
+const int SWITCHES_R[NUM_SWITCHES_R] = {
+  RING_L,
+  RING_H
+};
+const int SWITCHES_P[NUM_SWITCHES_P] = {
+  PINKY_L,
+  PINKY_H
+};
+
+/** END **/
+
+
+
 typedef short int boole;
 typedef int Key;
+typedef int FingerState;
 
 typedef struct {
-	boole index;
-	boole middle;
-	boole ring;
-	boole pinky;
+	FingerState index;
+	FingerState middle;
+	FingerState ring;
+	FingerState pinky;
 } Snapshot;
 
 typedef struct {
@@ -30,6 +74,52 @@ typedef struct {
 	SwitchHistory *history;
 	Output *outputM;
 } ClockReturn;
+
+
+Key *LAYOUT[LAYOUT_SIZE]; // treat as constant once created
+
+// REMOVE for use in Teensyduino
+int digitalRead(int x);
+int digitalRead(int x) {
+  return 0;
+}
+
+
+void handleOutOfMemory();
+void *myalloc(int size);
+
+Snapshot      *newSnapshotA (const FingerState i,
+                             const FingerState m,
+                             const FingerState r,
+                             const FingerState p);
+SwitchHistory *newHistoryA ();
+Output        *newOutputA ();
+
+void deleteSnapshotD (Snapshot *s);
+void deleteHistoryD (SwitchHistory *h);
+void deleteOutputD (Output *o);
+
+
+boole historyIsEmpty (const SwitchHistory *h);
+boole isRelease      (const Snapshot *s);
+boole historyTooLong (const SwitchHistory *h);
+
+int chordId (const Snapshot *s);
+
+Snapshot *chordFromM  (const SwitchHistory *h);
+Output   *outputForMA (const Snapshot *sM, const SwitchHistory *h);
+
+
+SwitchHistory *restartHistory (SwitchHistory *h);
+SwitchHistory *addToHistory   (Snapshot *sM, SwitchHistory *h);
+
+ClockReturn clockReturn (SwitchHistory *history, Output *output);
+ClockReturn clockA      (Snapshot *currentM, SwitchHistory *history);
+
+Snapshot *readInputsAIO ();
+void      sendOutputIO  (const Output *oM);
+
+
 
 void handleOutOfMemory()
 {
@@ -47,7 +137,7 @@ void *myalloc(int size)
 	return x;
 }
 
-Snapshot *newSnapshot(const boole i, const boole m, const boole r, const boole p)
+Snapshot *newSnapshotA(const FingerState i, const FingerState m, const FingerState r, const FingerState p)
 {
 	Snapshot *s = MALLOC(Snapshot);
 
@@ -59,7 +149,13 @@ Snapshot *newSnapshot(const boole i, const boole m, const boole r, const boole p
 	return s;
 }
 
-SwitchHistory *newHistory() {
+void deleteSnapshotD(Snapshot *s)
+{
+  free(s);
+}
+
+SwitchHistory *newHistoryA()
+{
 	SwitchHistory *h = MALLOC(SwitchHistory);
 
 	h->snapshots = MALLOCS(Snapshot *, MAX_SNAPSHOTS);
@@ -68,7 +164,18 @@ SwitchHistory *newHistory() {
 	return h;
 }
 
-Output *newOutput()
+void deleteHistoryD(SwitchHistory *h)
+{
+  int i;
+
+  for (i = 0; i < MAX_SNAPSHOTS; ++i) {
+    free(h->snapshots[i]);
+  }
+  free(h->snapshots);
+  free(h);
+}
+
+Output *newOutputA()
 {
 	Output *o = MALLOC(Output);
 	o->keys   = MALLOCS(Key,MAX_OUTPUT); //(Key *) malloc(MAX_OUTPUT * sizeof(Key));
@@ -76,19 +183,31 @@ Output *newOutput()
 	return o;
 }
 
-boole historyIsEmpty(SwitchHistory *h) {
+void deleteOutputD(Output *o)
+{
+  free(o->keys);
+  free(o);
+}
+
+
+
+boole historyIsEmpty(const SwitchHistory *h)
+{
 	return 0 == h->place;
 }
 
-boole isRelease(const Snapshot *s) {
+boole isRelease(const Snapshot *s)
+{
 	return s->pinky == 0 && s->ring == 0 && s->middle == 0 && s->index == 0;
 }
 
-boole historyTooLong(const SwitchHistory *h) {
+boole historyTooLong(const SwitchHistory *h)
+{
 	return h->place == MAX_SNAPSHOTS;
 }
 
-Snapshot *chordFromM(SwitchHistory *h) {
+Snapshot *chordFromM(const SwitchHistory *h)
+{
 	if (h->place == 0) {
 		return NULL;
 	} else {
@@ -97,7 +216,9 @@ Snapshot *chordFromM(SwitchHistory *h) {
 }
 
 
-SwitchHistory *restartHistory(SwitchHistory *h) {
+// CHANGES h
+SwitchHistory *restartHistory(SwitchHistory *h)
+{
 	h->place = 0;
 	return h;
 }
@@ -123,18 +244,18 @@ SwitchHistory *addToHistory(Snapshot *sM, SwitchHistory *h)
 	return h;
 }
 
-int chordId(Snapshot *s)
+int chordId(const Snapshot *s)
 {
 	return s->index + 2*s->middle + 4*s->ring + 8*s->pinky;
 }
 
-Output *outputForM(Snapshot *sM, SwitchHistory *h)
+Output *outputForMA(const Snapshot *sM, const SwitchHistory *h)
 {
 	if (NULL != sM && isRelease(sM)) {
 		Snapshot *chordM = chordFromM(h);
 
 		if (NULL != chordM) {
-			Output *o = newOutput();
+			Output *o = newOutputA();
 			o->keys = LAYOUT[chordId(chordM)];
 
 			return o;
@@ -149,25 +270,44 @@ Output *outputForM(Snapshot *sM, SwitchHistory *h)
 ClockReturn clockReturn(SwitchHistory *history, Output *output)
 {
 	ClockReturn cr = {history, output};
+
 	return cr;
 }
 
-ClockReturn clock(Snapshot *currentM, SwitchHistory *history) {
-	Output  *oM;
+ClockReturn clockA(Snapshot *currentM, SwitchHistory *history)
+{
 	SwitchHistory *new_h;
+	Output *oM;
 
 	new_h = addToHistory(currentM, history);
-	oM    = outputForM(currentM, new_h);
+	oM    = outputForMA(currentM, new_h);
 
 	return clockReturn(new_h, oM);
 }
 
 
-Snapshot *readInputsIO() {
-	// TODO
+Snapshot *readInputsAIO()
+{
+  int i, multiplier;
+  FingerState index = 0, middle = 0, ring = 0, pinky = 0;
+
+  for (i = 0; i < NUM_SWITCHES_I; ++i) {
+    index  = index  << 1 + digitalRead(SWITCHES_I[i]);
+  }
+  for (i = 0; i < NUM_SWITCHES_M; ++i) {
+    middle = middle << 1 + digitalRead(SWITCHES_M[i]);
+  }
+  for (i = 0; i < NUM_SWITCHES_R; ++i) {
+    ring   = ring   << 1 + digitalRead(SWITCHES_R[i]);
+  }
+  for (i = 0; i < NUM_SWITCHES_P; ++i) {
+    pinky  = pinky  << 1 + digitalRead(SWITCHES_P[i]);
+  }
+
+  return newSnapshotA(index, middle, ring, pinky);
 }
 
-void sendOutputIO(Output *oM)
+void sendOutputIO(const Output *oM)
 {
 	if (NULL != oM) {
 		// TODO
@@ -181,18 +321,24 @@ void sendOutputIO(Output *oM)
 /***** MAIN PROCESSES BELOW HERE *****/
 
 SwitchHistory *history_GLOBAL;
-Key LAYOUT[LAYOUT_SIZE]; // treat as constant once created
 
 void setup() {
+  // this allocation is never freed
+  history_GLOBAL = newHistoryA();
+
 	// TODO: create LAYOUT from config
 }
 
 void loop() {
-	ClockReturn r = clock(readInputsIO(), history_GLOBAL);
+	ClockReturn r = clockA(readInputsAIO(), history_GLOBAL);
 
 	sendOutputIO(r.outputM);
 
+  // set for next loop
 	history_GLOBAL = r.history;
+
+  // cleanup memory
+  deleteHistoryD(r.history);
 }
 
 
